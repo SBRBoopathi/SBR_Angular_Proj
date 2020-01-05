5c3ccd7819c995aae08066d48dc694d0e0c05be9
SBRBoopathi <boopathi15it@gmail.com>
2020-01-05 17:06:10 +0530

    9 files modified, 16 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,30 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
 
 /************* DropDown  **************/
 
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, NgbModule, RouterModule.forRoot(appRoutes)],
   exports: [RouterModule, HeaderComponent],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, HeaderComponent],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, HeaderComponent, BannerComponent, AboutComponent, SkillsComponent, ContactComponent, ExperienceComponent],
   providers: [],
-  bootstrap: [AppComponent, HeaderComponent]
+  bootstrap: [AppComponent, HeaderComponent, BannerComponent, AboutComponent, SkillsComponent, ContactComponent, ExperienceComponent]
 })
 export class AppModule { }
