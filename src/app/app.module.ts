dea5a08e5ca92f4507b1ddde38b007b15270a9d7
SBRBoopathi <boopathi15it@gmail.com>
2020-01-05 11:52:10 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+/************* DropDown  **************/
+
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent }
@@ -13,12 +19,10 @@ const appRoutes: Routes = [
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  exports: [
-  RouterModule
-  ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent],
+  imports:      [ BrowserModule, FormsModule, NgbModule, RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule, HeaderComponent],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, HeaderComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeaderComponent]
 })
 export class AppModule { }
