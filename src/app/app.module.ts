105b9e8f2b1b7092434b32d7bd1ac683ef8a4cd4
SBRBoopathi <boopathi15it@gmail.com>
2020-01-12 19:26:17 +0530

    7 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+/*************************ng Bootrap ************/
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import { NgbdTablePagination } from './table-pagination';
+
+/*************************ng Bootrap ************/
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -29,7 +34,7 @@ const appRoutes: Routes = [
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, NgbModule, RouterModule.forRoot(appRoutes)],
+  imports:      [ BrowserModule, FormsModule, NgbModule, RouterModule.forRoot(appRoutes), HttpClientModule],
   exports: [RouterModule, HeaderComponent],
   declarations: [ AppComponent, HelloComponent, HomeComponent, HeaderComponent, BannerComponent, AboutComponent, SkillsComponent, ContactComponent, ExperienceComponent],
   providers: [],
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'skills',
   templateUrl: './skills.component.html',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent  {
   name = 'skills';
+  constructor(private http:HttpClient){}
+  myskills : object;
+  ngOnInit(){
+    this.http.get('https://api.myjson.com/bins/16i7x4').subscribe(data => {
+      this.myskills = data;
+      console.log(data)
+    })
+  }
+
+
+  
+
+  /*page = 1;
+  pageSize = 4;
+  collectionSize = COUNTRIES.length;
+
+  get countries(): Country[] {
+    return COUNTRIES
+      .map((country, i) => ({id: i + 1, ...country}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }*/
 }
