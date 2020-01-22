9e213ba127689841a5a25b18f89143cc52ad4a95
SBRBoopathi <boopathi15it@gmail.com>
2020-01-22 19:06:59 +0530

    1 file modified

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { ChildComponent } from "../child/child.component";
 @Component({
   selector: 'app-parent',
@@ -22,7 +22,15 @@ export class ParentComponent implements AfterViewInit{
   viewChildMsg : string;
 
   // 1) View Child example of passing mesage text strats here ///
+  
+  // 2) View Child example template ref dom update /// 
   @ViewChild('desct', {static:false}) desct : ElementRef;
+  // 2) View Child example of passing mesage text Ends here ///
+
+
+  // 3) View Children example selecting multiple childrens starts here /// ViewChildren to get the QueryList of elements or directives from the view DOM.//
+  @ViewChildren(ChildComponent) hellos: QueryList<any>;
+  // 3) View Children example selecting multiple childrens Ends here /// 
 
 
    ngAfterViewInit(){
@@ -30,6 +38,8 @@ export class ParentComponent implements AfterViewInit{
 
      console.log(this.desct.nativeElement.innerHTML);
      this.desct.nativeElement.innerHTML = "Using View child (elementref) Dome succesfully updated"
+
+     this.hellos.forEach(hello => console.log(hello));
    }
 
 
